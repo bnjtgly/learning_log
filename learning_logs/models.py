1edaf4c6d476323f6947860b18ab24b2a09be6fe
= <benjie.tigley@nelnetphilippines.com>
2024-05-13 16:50:40 +0800

    Added Entry model.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,7 +5,20 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        """Return a string representation of the Model"""
+        return self.text
 
-def __str__(self):
-    """Return a string representation of the Model"""
-    return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the Model"""
+        return f"{self.text[:50]}"
