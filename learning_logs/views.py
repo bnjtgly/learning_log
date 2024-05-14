32a70114acaf9b3e6847bd0ffbe286df8f117786
= <benjie.tigley@nelnetphilippines.com>
2024-05-14 09:58:51 +0800

    Added html templates.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h3>Welcome to Learning Log!<h3>")
+    return render(request, 'index.html')
